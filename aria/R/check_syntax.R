1e971e1bc8255c4379fe0fbc928fbea94542ded3
Mike Lawrence <mike@axemneuro.com>
2022-02-18 19:26:38 -0400

    refactoring syntax_ignore

--- aria/R/check_syntax.R
+++ aria/R/check_syntax.R
@@ -15,10 +15,13 @@ check_syntax = function(aria_args){
 		, wd = fs::path_dir(aria_args$code_path)
 		, error_on_status = F
 	)
-	stderr = stanc_syntax_check_run$stderr
-	if((stderr!='')&(!is.null(aria_args$syntax_ignore))){
+	syntax_check_passed = TRUE
+	if(stanc_syntax_check_run$stderr==''){
+		cat(aria:::blue('  ✓ Syntax check passed\n'))
+	}else{
+		# join entries
 		(
-			stderr
+			stanc_syntax_check_run$stderr
 			%>% stringr::str_split('\n')
 			%>% purrr::pluck(1)
 			%>% stringi::stri_remove_empty()
@@ -35,31 +38,49 @@ check_syntax = function(aria_args){
 			%>% stringr::str_replace_all('\\h+',' ')
 		) ->
 			ew_entries
-		stderr_vec = NULL
-		for(entry in ew_entries){
-			toss = FALSE
-			for(ignore_string in aria_args$syntax_ignore){
-				if(stringr::str_detect(entry,ignore_string)){
-					toss = TRUE
-				}
+		#split out errors
+		error_list = ew_entries[stringr::str_starts(ew_entries,'Error')]
+		if(length(error_list)>0){
+			syntax_check_passed = FALSE
+			if(!getOption('aria_sotto_vocce')){
+				beepr::beep(system.file("sounds/critical_stop.wav", package="aria"))
 			}
-			if(!toss){
-				stderr_vec = c(stderr_vec,entry)
-			}
+			cat(aria:::red('Stan syntax-check encountered the following ERRORS:'),'\n',sep='')
+			cat(aria:::red(paste(error_list,collapse='\n \n')),'\n \n',sep='')
 		}
-		stderr = paste(stderr_vec,collapse='\n')
+		#split out warnings & check if in aria_args$syntax_ignore
+		(
+			ew_entries[stringr::str_starts(ew_entries,'Warning')]
+			%>% tibble::tibble(entry=.)
+			%>% dplyr::mutate(
+				briefer_entry = stringr::str_remove_all(entry,'^Warning.*: ')
+				, entry_in_ignore = briefer_entry %in% aria_args$syntax_ignore
+			)
+		) ->
+			warnings_tbl
+		warnings_to_print = (warnings_tbl %>% dplyr::filter(!entry_in_ignore) %>% dplyr::pull(entry))
+		if(length(warnings_to_print)>0){
+			syntax_check_passed = FALSE
+			if(!getOption('aria_sotto_vocce')){
+				beepr::beep(system.file("sounds/critical_stop.wav", package="aria"))
+			}
+			cat(aria:::red('Stan syntax-check encountered the following WARNINGS:\n \n'))
+			cat(aria:::red(paste(warnings_to_print,collapse='\n \n')),'\n \n \n',sep='')
+			cat(aria:::blue("You should probably fix the source of these warnings, but if you feel they are false-alarms and want to instead simply ignore them, add the following to the top of your .stan file:\n \n"))
+			(
+				warnings_tbl
+				%>% dplyr::pull(briefer_entry)
+				%>% dQuote(q=F)
+				%>% paste(collapse=', ')
+				%>% paste0('//aria: syntax_ignore = c(',.,')')
+			) ->
+				aria_syntax_ignore_recommendation
+			cat(aria_syntax_ignore_recommendation,'\n \n \n',sep='')
+		}
 	}
-	syntax_check_passed = !((stderr!='')|(stanc_syntax_check_run$stdout!=''))
-	if(!syntax_check_passed){
-		if(!getOption('aria_sotto_vocce')){
-			beepr::beep(system.file("sounds/critical_stop.wav", package="aria"))
-		}
-		stanc_syntax_check_run$stdout = stringr::str_remove_all(stanc_syntax_check_run$stdout,stringr::fixed('./'))
-		stanc_syntax_check_run$stdout = stringr::str_replace_all(stanc_syntax_check_run$stdout, 'Info: ', '\nInfo:\n')
-		cat(aria:::blue(stanc_syntax_check_run$stdout),'\n\n',sep='')
-		cat(aria:::red(stanc_syntax_check_run$stderr),'\n',sep='')
-	}else{
-		cat(aria:::blue('  ✓ Syntax check passed\n'))
+	if(stanc_syntax_check_run$stdout!=''){
+		cat(aria:::blue('Stan syntax-check encountered the following informational messages:\n \n'))
+		cat(stanc_syntax_check_run$stdout)
 	}
 	if(sys.parent()==0){ #function is being called from the global env
 		return(invisible(NULL))
